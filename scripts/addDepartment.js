aa48b4d61aea1c46c1b6a70101f7585109dab441
Marina H <mxhuisken@gmail.com>
2022-07-19 19:41:35 -0700

    addDepartment

---
+++ scripts/addDepartment.js
@@ -0,0 +1,24 @@
+// Import Modules
+const db = require('../config/connection');
+const inquirer = require('inquirer');
+
+
+async function addDepartment() {
+    try {
+        const { department_name } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: "department_name",
+                message: "Enter Department Name:",
+            },
+        ])
+        
+        console.log(department_name)
+        await db.promise().query(`INSERT INTO departments (department_name) VALUES (?)`, [department_name])
+        return `${department_name} has been added to the database`
+    } catch (err) {
+        console.log(`Opps! Something went wrong...`, err)
+    }
+}
+
+module.exports = addDepartment;
